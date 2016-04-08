64f8a0633b9384ad70c194117b328b285076fbca
chutch01 <chutch01@hotmail.com>
2016-04-08 11:22:57 -0400

    Added splash screen

---
+++ QuickMemo/app/src/main/java/ca/georgiancollege/quickmemo/splashScreen.java
@@ -0,0 +1,39 @@
+package ca.georgiancollege.quickmemo;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by vamsikrishna on 12-Feb-15.
+ */
+public class splashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(splashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
