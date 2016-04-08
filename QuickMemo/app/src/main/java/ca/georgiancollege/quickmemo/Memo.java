0ca53bb076f9635456fddcedbb057ad521ff1778
BlaineParr <blaine.parr@hotmail.com>
2016-04-08 11:25:49 -0400

    Added Memo class.

---
+++ QuickMemo/app/src/main/java/ca/georgiancollege/quickmemo/Memo.java
@@ -0,0 +1,48 @@
+package ca.georgiancollege.quickmemo;
+
+/**
+ * Created by spike on 2016-04-08.
+ */
+public class Memo
+{
+    private String _title;
+    private String _category;
+    private String _description;
+
+    public Memo()
+    {
+        this._title = "";
+        this._category = "";
+        this._description = "";
+    } //constructor ends
+
+    public String getTitle()
+    {
+        return this._title;
+    } //method getTitle ends
+
+    public String getCategory()
+    {
+        return this._category;
+    } //method getCategory ends
+
+    public String getDescription()
+    {
+        return this._description;
+    } //method getDescription ends
+
+    public void setTitle(String title)
+    {
+        this._title = title;
+    } //method setTitle ends
+
+    public void setCategory(String category)
+    {
+        this._category = category;
+    } //method setCategory ends
+
+    public void setDescription(String description)
+    {
+        this._description = description;
+    } //method setDescription ends
+} //class Memo ends
